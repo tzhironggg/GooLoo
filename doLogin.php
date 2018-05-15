06e6237beb05422abfa72af3f2a044027ba88165
16022671 <16022671@myrp.edu.sg>
2018-05-15 22:33:03 +0800

    Login,Register,Logout done - Norman Update 15/5/2018 22:32

---
+++ doLogin.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+include 'dbFunctions.php';
+$msg = "";
+
+if (!isset($_SESSION['username'])) {
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+        //retrieve form data
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        //match the username and password entered with database record
+        $loginQuery = "SELECT * FROM customer WHERE password = '$password' AND(email = '$username' OR mobile = '$username')";
+        $result = mysqli_query($link, $loginQuery) or die(mysqli_error($link));
+
+        //if record is found, store id and username into session
+        if (mysqli_num_rows($result) == 1) {
+            $row = mysqli_fetch_array($result);
+           
+            
+           
+        } else {
+            $msg = "Sorry, you must enter a valid username 
+                    and password to log in.<br/><a href='login.php'>Back</a>";
+            echo $msg;
+        }
+    }
+} else {
+    $msg = "You are are already logged in.";
+}
+?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+
+    </body>
+</html>
